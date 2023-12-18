1094a7c560a4db96e56c1b5d7f74fb69cfb64b3d
narimiran <narimiran@disroot.org>
2023-12-18 17:44:58 +0100

    day18.clj

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ Day 00: Helper file                           | [aoc.clj](clojure/aoc.clj)     |
 [Day 15](http://adventofcode.com/2023/day/15) | [day15.clj](clojure/day15.clj) | Advent of Reading.
 [Day 16](http://adventofcode.com/2023/day/16) | [day16.clj](clojure/day16.clj) | Is this a cousin of Day 10?
 [Day 17](http://adventofcode.com/2023/day/17) | [day17.clj](clojure/day17.clj) | Consider only L/R directions, and then go straight as far as you can.
-<!-- [Day 18](http://adventofcode.com/2023/day/18) | [day18.clj](clojure/day18.clj) | -->
+[Day 18](http://adventofcode.com/2023/day/18) | [day18.clj](clojure/day18.clj) | TIL about Shoelace formula and Pick's theorem.
 <!-- [Day 19](http://adventofcode.com/2023/day/19) | [day19.clj](clojure/day19.clj) | -->
 <!-- [Day 20](http://adventofcode.com/2023/day/20) | [day20.clj](clojure/day20.clj) | -->
 <!-- [Day 21](http://adventofcode.com/2023/day/21) | [day21.clj](clojure/day21.clj) | -->
---
+++ clojure/day18.clj
@@ -0,0 +1,43 @@
+(ns day18
+  (:require aoc
+            [clojure.string :as str]))
+
+
+(def deltas {"R" [1 0] "D" [0 1] "L" [-1 0] "U" [0 -1]
+             "0" [1 0] "1" [0 1] "2" [-1 0] "3" [0 -1]})
+
+
+(defn parse-line [line]
+  (let [[d n c] (str/split line #" ")
+        c (str/replace c #"[(#)]" "")
+        [dist dir] (map #(apply str %) (split-at 5 c))
+        dist (Integer/parseInt dist 16)]
+    {:p1 {:dir  (deltas d)
+          :dist (parse-long n)}
+     :p2 {:dir  (deltas dir)
+          :dist dist}}))
+
+
+(defn dig-trench [input]
+  ; Shoelace formula + Pick's theorem
+  (->> input
+       (reduce (fn [{:keys [total x]}
+                    {:keys [dir dist]}]
+                 (let [[dx dy] dir
+                       nx (+ x (* dist dx))
+                       area (* nx (* dist dy))]
+                   {:x nx
+                    :total (+ total area (/ dist 2))}))
+               {:total 1
+                :x 0})
+       :total
+       long))
+
+
+(defn solve [input-file]
+  (let [dig-plan (aoc/read-input input-file parse-line)]
+    [(dig-trench (map :p1 dig-plan))
+     (dig-trench (map :p2 dig-plan))]))
+
+
+(solve 18)
