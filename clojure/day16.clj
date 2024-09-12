21123074cf23ea3aac487668931d5a25ed0ad53b
narimiran <narimiran@disroot.org>
2024-09-12 15:51:37 +0200

    day16.clj: 2.5x faster

--- clojure/day16.clj
+++ clojure/day16.clj
@@ -3,38 +3,57 @@
 
 
 (defn conj' [col [a b :as xs]]
-  ; to make the main function a bit nicer
+  ;; to make the main function a bit nicer
   (if (int? a)
     (conj col xs)
-    (conj (conj col a) b)))
+    (conj col a b)))
 
+(defn my-hash [x y dx dy]
+  ;; 2x faster than the built-in `hash`
+  (+ (* 12345 x)
+     (* 1234 y)
+     (* 123 dx)
+     dy))
+
+(def ^:dynamic seen-starts)
 
 (defn traverse [contraption x y dx dy]
-  (let [size (count contraption)]
-    (loop [seen      (transient #{})
-           energized (transient #{})
-           stack     [[x y dx dy]]]
-      (if-let [[x y dx dy :as current] (peek stack)]
-        (let [h (hash current)]
-          (if (or (not (< -1 x size))
-                  (not (< -1 y size))
-                  (seen h))
-            (recur seen energized (pop stack))
-            (recur
-             (conj! seen h)
-             (conj! energized (+ x (* size y)))
-             (conj' (pop stack)
-                    (case ((contraption y) x)
-                      \. [(+ x dx) (+ y dy) dx dy]
-                      \/ [(- x dy) (- y dx) (- dy) (- dx)]
-                      \\ [(+ x dy) (+ y dx) dy dx]
-                      \| (if (zero? dx)
-                           [x (+ y dy) 0 dy]
-                           [[x (dec y) 0 -1] [x (inc y) 0 1]])
-                      \- (if (zero? dy)
-                           [(+ x dx) y dx 0]
-                           [[(dec x) y -1 0] [(inc x) y 1 0]]))))))
-        (count energized)))))
+  (if (@seen-starts [x y]) 0
+      (let [size (count contraption)]
+        (loop [seen      (transient #{})
+               energized (transient #{})
+               stack     (list [x y dx dy])]
+          (if-let [[x y dx dy] (peek stack)]
+            (let [h (my-hash x y dx dy)]
+              (cond
+                (not (< -1 x size))
+                (do
+                  (swap! seen-starts conj [(- x dx) y])
+                  (recur seen energized (pop stack)))
+
+                (not (< -1 y size))
+                (do
+                  (swap! seen-starts conj [x (- y dy)])
+                  (recur seen energized (pop stack)))
+
+                (seen h)
+                (recur seen energized (pop stack))
+
+                :else
+                (recur (conj! seen h)
+                       (conj! energized (+ x (* size y)))
+                       (conj' (pop stack)
+                              (case ((contraption y) x)
+                                \. [(+ x dx) (+ y dy) dx dy]
+                                \/ [(- x dy) (- y dx) (- dy) (- dx)]
+                                \\ [(+ x dy) (+ y dx) dy dx]
+                                \| (if (zero? dx)
+                                     [x (+ y dy) 0 dy]
+                                     [[x (dec y) 0 -1] [x (inc y) 0 1]])
+                                \- (if (zero? dy)
+                                     [(+ x dx) y dx 0]
+                                     [[(dec x) y -1 0] [(inc x) y 1 0]]))))))
+            (count energized))))))
 
 
 (defn max-energy [contraption]
@@ -51,8 +70,9 @@
 
 (defn solve [input]
   (let [contraption (aoc/parse-input input :chars)]
-    [(traverse contraption 0 0 1 0)
-     (max-energy contraption)]))
+    (binding [seen-starts (atom #{})]
+      [(traverse contraption 0 0 1 0)
+       (max-energy contraption)])))
 
 
 (solve (aoc/read-file 16))
